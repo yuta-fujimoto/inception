a870cf20abd95b5bbc3a5fe818f66f32b8e4a381
fyuta <fyuta@student.42tokyo.jp>
2022-08-15 19:16:05 +0900

    refactor

--- srcs/requirements/mariadb/tool/configure.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
-
-chown -R mysql:mysql /var/lib/mysql
-chmod -R 700 /var/lib/mysql
-
-exec /usr/bin/mysqld --user=mysql --console
